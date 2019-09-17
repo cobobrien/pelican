c703b29ef1e48e5a7fb7f4c43e5e59c4a9939833
cobobrien <ccfobrien@gmail.com>
2019-09-17 15:26:29 +0100

    initial

---
+++ services/users/project/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
+
+# instantiate the app
+app = Flask(__name__)
+
+api = Api(app)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+        'status': 'success',
+        'message': 'pong!'
+    }
+
+api.add_resource(UsersPing, '/users/ping')
