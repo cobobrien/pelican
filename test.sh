ec66726bbd33b6ef3e799ed11f42b0a08711746d
cobobrien <ccfobrien@gmail.com>
2019-09-26 16:05:52 +0100

    change of localhost in test script

--- test.sh
+++ test.sh
@@ -22,7 +22,7 @@ docker-compose down
 # run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://192.168.99.100
+./node_modules/.bin/cypress run --config baseUrl=http://127.0.0.1
 inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
 
