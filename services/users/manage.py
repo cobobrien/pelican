2ea7434ae1cd6c9a55fb50a75944e405c213304b
cobobrien <ciaran@mywallst.com>
2020-05-15 12:45:58 +0100

    config testing and DB initiation

---
+++ services/users/manage.py
@@ -0,0 +1,26 @@
+from flask.cli import FlaskGroup
+import sys
+import unittest
+from project import app, db
+
+
+cli = FlaskGroup(app)
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
+
+if __name__ == '__main__':
+    cli()
