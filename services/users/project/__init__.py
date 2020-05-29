ab6ef8ab511197c851d921d1269be560b20cf18e
cobobrien <ciaran@mywallst.com>
2020-05-29 14:31:00 +0100

    react components setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,11 +3,14 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+cors = CORS()
+
 
 def create_app(script_info=None):
 
@@ -21,6 +24,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    cors.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
